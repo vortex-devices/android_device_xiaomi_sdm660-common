2b70703692b1aa1c46b57485b49de97ec5f86c2a
Michael Lenskiy <lmichael1003@gmail.com>
2020-03-04 13:51:11 +0100

    sdm660: render tweaks
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- common_prop.mk
+++ common_prop.mk
@@ -131,7 +131,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=0 \
+    debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.disable_backpressure= 1 \
@@ -141,7 +141,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.enable_gl_backpressure=1 \
     debug.cpurend.vsync=false \
-    debug.hwui.use_buffer_age=false \
+    debug.hwui.use_buffer_age=true \
     persist.debug.wfd.enable=1 \
     persist.hwc.enable_vds=1 \
     ro.opengles.version=196610 \
